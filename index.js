d12f6d4d9bc06f9fc291cc27fa8fccd6cee7bb71
CSFernandoHBK <fernandotam2@hotmail.com>
2022-11-06 23:33:14 -0300

    logando na conta e mostrando tweets

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const tweets = [{username: "fernando", tweet: "meu nome eh esse mesmo"},
-{username: "ata", tweet: "meu nome eh esse mesmo"},
-{username: "tata", tweet: "meu nome eh esse mesmo"}]
+const dadosUsuarios = [
+    {username: "frodo", avatar:"https://static.wikia.nocookie.net/senhordosaneisemportugues/images/e/e6/ImagesCADWY2QL.jpg/revision/latest?cb=20120106073318&path-prefix=pt-br"},
+    {username: "bobesponja", avatar: "https://yt3.ggpht.com/ytc/AMLnZu9tYPIG3bxki2LZz-NRrvHtLHRL0-wW95Cjgcr2=s900-c-k-c0x00ffffff-no-rj"},
+    {username: "naruto", avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"}
+];
 
-app.post("/sign-up", (req, res) => {console.log(req.body)})
+const tweets = [{username: "frodo", tweet: "vamos atras do anel neh"},
+{username: "bobesponja", tweet: "hamburger de siri"},
+{username: "naruto", tweet: "meu jeito ninja de ser"}]
+
+app.post("/sign-up", (req, res) => {
+    const dadosUser = req.body;
+    dadosUsuarios.push(dadosUser);
+    res.send(dadosUser)
+})
+
+app.post("/tweets", (req, res) => {
+    
+})
 
 app.get("/tweets", (req, res) => {
     res.send(tweets)})
